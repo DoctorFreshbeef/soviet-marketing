6b775e9eeb6bb750965a805acbddfee4a05303ef
Dr. Freshbeef <rob@robzeeman.nl>
2021-12-21 23:22:46 +0100

    First raw version

---
+++ js/sm.js
@@ -0,0 +1,30 @@
+var i = 1;
+var audio = new Audio('audio/sm_sound.mp3');
+
+function play(){
+    audio.loop = true;
+    audio.play();
+    setTimeout( next, 10000);
+}
+
+function next() {
+    i++;
+    if (i < 31) {
+        $("#frame").fadeOut(4000, function () {
+            $("#frame").attr("src", "img/img" + i + ".png");
+            $("#frame").fadeIn(5000);
+            setTimeout( next, 6000);
+        });
+    }else{
+        setTimeout(fade, 500);
+    } 
+}
+
+function fade(){
+    audio.volume = audio.volume * 0.9;
+    if (audio.volume < 0.05){
+        audio.pause();
+    }else{
+        setTimeout(fade, 500);
+    }
+}
